7c416aaf80361b5d021899cd5055f2a23624f941
benmurray <benmurray573@gmail.com>
2025-03-15 15:07:47 +0000

    Create home page template
    Add bottom nav bar with icons
    Add top nav bar with title and login button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,19 +25,83 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  // var current = WordPair.random();
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 16, 36, 90),
+                const Color.fromARGB(255, 72, 51, 165)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.fitness_center, size: 30, color: Colors.white),
+        ),
+        title: Text(
+          'Gym Buddy',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blueGrey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Text('Login'),
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Chest Day 1'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+                color: Colors.blueGrey,
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.sports_gymnastics_rounded,
+                size: 30,
+                color: Colors.blueGrey,
+              ),
+              label: "Workouts"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+                size: 30,
+                color: Colors.blueGrey,
+              ),
+              label: "Settings"),
         ],
       ),
     );
